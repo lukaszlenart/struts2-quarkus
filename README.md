12ae9a8769b21aa3345c64eb7cf8940c3128f627
Lukasz Lenart <lukasz.lenart@gmail.com>
2020-11-19 09:15:12 +0100

    Initial Struts2 setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Running Struts 2 with Quarkus
+
+This is an example application how to run [Apache Struts 2](https://struts.apache.org/) based application using
+[Quarkus](https://quarkus.io/).
---
+++ src/main/java/org/apache/struts2/quarkus/IndexAction.java
@@ -0,0 +1,11 @@
+package org.apache.struts2.quarkus;
+
+import com.opensymphony.xwork2.Action;
+
+public class IndexAction {
+
+    public String execute() {
+        return Action.SUCCESS;
+    }
+
+}
